ce45f91ff5466a2a679331a88d7bd0fe89882779
Derek Chafin <infomaniac50@gmail.com>
2011-03-08 10:16:30 -0600

    added test page to global event plugin

---
+++ GlobalEvent/jquery.globalEvent.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+
+    var eventHolder = {};
+
+    $.fn.globalEvent = {
+        bind: function (eventName, func) {
+            $(eventHolder).bind(eventName, this, function (e) {
+                func.call(e.data);
+            });
+        }
+    };
+
+    $.extend({
+        globalEvent: {
+            trigger: function (eventName) {
+                $(eventHolder).trigger(eventName);
+            }
+        }
+    });
+
+})(jQuery);
